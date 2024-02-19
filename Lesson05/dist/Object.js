50df2aff7489321db31a437b05c88e58c0164f2d
Vitor Freitas <vitor.lucfreitas@gmail.com>
2024-02-19 18:18:08 +0000

    Adds Lesson05 folder

---
+++ Lesson05/dist/Object.js
@@ -0,0 +1,10 @@
+"use strict";
+let employee = {
+    id: 1,
+    name: "Vitor",
+    doTask: (task) => {
+        console.log(task + " in progress");
+    }
+};
+employee.doTask("Remove bugs");
+//# sourceMappingURL=Object.js.map
---
+++ Lesson05/src/Object.ts
@@ -0,0 +1,32 @@
+// Objects
+
+// Blueprint
+type Employee = {
+    // Attribute
+    readonly id:number, // Forbid to be changed
+    name: string
+    // Methods
+    doTask: (task: string) => void
+}
+
+let employee1: Employee = {
+    id:1, 
+    name: "Vitor",
+    doTask: (task: string ) => {
+        console.log(task + " in progress");
+        
+    }
+};
+let employee2: Employee = {
+    id:1, 
+    name: "Marcela",
+    doTask: (task: string ) => {
+        console.log(task + " in progress");
+        
+    }
+};
+
+employee1.doTask("Remove bugs");
+console.log(employee2.name);
+
+
